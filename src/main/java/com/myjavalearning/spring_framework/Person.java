645e7212fc40304c3b8d6f8edaa3c88e1ac56174
Femi Ashiru <femi.ashiru@bjss.com>
2024-11-16 18:47:06 +0000

    add a new record

---
+++ src/main/java/com/myjavalearning/spring_framework/Person.java
@@ -0,0 +1,13 @@
+package com.myjavalearning.spring_framework;
+
+// A record is a new feature introduced in JDK 16
+// A record is a special type of class designed to hold immutable data (no setters).
+// It automatically generates methods like constructors, getters, equals(), hasCode() and toString()
+// reducing boilerplate code.
+
+// Records are ideal for simple data carriers, making code cleaner and more concise
+
+public record Person(String name, int age) {
+    // record does not need a constructor
+    // record does not need getters
+}
