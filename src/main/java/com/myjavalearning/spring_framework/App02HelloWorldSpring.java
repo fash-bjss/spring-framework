0fe71f85f91eb125acce31e377147f8f0b298f0a
Femi Ashiru <femi.ashiru@bjss.com>
2024-11-16 18:01:03 +0000

    retireve Beans being managed by Spring
    we can use the context.getBean(<name_of_bean>) to get a specific bean managed by Spring

--- src/main/java/com/myjavalearning/spring_framework/App02HelloWorldSpring.java
+++ src/main/java/com/myjavalearning/spring_framework/App02HelloWorldSpring.java
@@ -8,6 +8,17 @@ public class App02HelloWorldSpring {
         // 1: Launch a spring context
         AnnotationConfigApplicationContext context
                 =  new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
+
         // 2: Configure the things we want spring framework to manage
+        // created HelloWorldConfiguration - @Configuration
+        // created a spring bean called name - @Bean
+
+        // 3: Now we are retrieving the Beans managed by Spring
+        // In order for us to see what beans Spring is managing we can use the .getBean() method
+        // calling it on the context created above context.getBean()
+        Object theBean = context.getBean("name");
+
+        System.out.println(theBean);
+
     }
 }
