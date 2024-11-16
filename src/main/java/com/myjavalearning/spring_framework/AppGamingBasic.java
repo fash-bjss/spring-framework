66dbb6ab692d01e31eb58adc34046c90367792c7
Femi Ashiru <femi.ashiru@bjss.com>
2024-11-16 16:32:04 +0000

    We have an error here
    This is due to tight coupling

--- src/main/java/com/myjavalearning/spring_framework/AppGamingBasic.java
+++ src/main/java/com/myjavalearning/spring_framework/AppGamingBasic.java
@@ -4,7 +4,9 @@ import com.myjavalearning.spring_framework.game.*;
 public class AppGamingBasic {
     public static void main(String[] args){
         MarioGame marioGame = new MarioGame();
-        GameRunner gameRunner = new GameRunner(marioGame);
+        SuperContra superContra = new SuperContra();
+
+        GameRunner gameRunner = new GameRunner(superContra);
 
         gameRunner.run();
     }
---
+++ src/main/java/com/myjavalearning/spring_framework/game/SuperContra.java
@@ -0,0 +1,20 @@
+package com.myjavalearning.spring_framework.game;
+
+public class SuperContra {
+
+    public void up(){
+        System.out.println("up");
+    }
+
+    public void down(){
+        System.out.println("Sit down");
+    }
+
+    public void left(){
+        System.out.println("Back");
+    }
+
+    public void right(){
+        System.out.println("Shoot a bullet");
+    }
+}
