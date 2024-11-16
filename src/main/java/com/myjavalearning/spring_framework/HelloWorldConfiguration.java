be15c9e89b75538793057226fc68dc75c9b680d2
Femi Ashiru <femi.ashiru@bjss.com>
2024-11-16 17:39:48 +0000

    add a configuration class
    configuration class used to configure the things that we want the spring framework to manage

---
+++ src/main/java/com/myjavalearning/spring_framework/HelloWorldConfiguration.java
@@ -0,0 +1,8 @@
+package com.myjavalearning.spring_framework;
+//To indicate that this is a configuration class we add the @Configuration annotation
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HelloWorldConfiguration {
+}
