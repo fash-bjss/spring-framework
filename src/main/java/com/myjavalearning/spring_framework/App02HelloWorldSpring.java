0d368f865b33862eb4c5fa90dffabe99148fa632
Femi Ashiru <femi.ashiru@bjss.com>
2024-11-16 17:50:43 +0000

    we add AnnotationConfigApplicationContext to launch the HelloWorldConfiguration class

--- src/main/java/com/myjavalearning/spring_framework/App02HelloWorldSpring.java
+++ src/main/java/com/myjavalearning/spring_framework/App02HelloWorldSpring.java
@@ -1,8 +1,13 @@
 package com.myjavalearning.spring_framework;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class App02HelloWorldSpring {
     public static void main(String[] args){
         // 1: Launch a spring context
+        AnnotationConfigApplicationContext context
+                =  new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
         // 2: Configure the things we want spring framework to manage
     }
 }
