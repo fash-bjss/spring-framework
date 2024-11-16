8c6a1e8b660cb28f07fd475da26adf9d6fe33a01
Femi Ashiru <femi.ashiru@bjss.com>
2024-11-16 17:26:52 +0000

    we mange it but we want spring to
    currently we are responsible for writing the objects and injecting the dependencies, however we want to get to a stage where the Spring Framework is handling this for us

--- src/main/java/com/myjavalearning/spring_framework/AppGamingBasic.java
+++ src/main/java/com/myjavalearning/spring_framework/AppGamingBasic.java
@@ -6,8 +6,19 @@ public class AppGamingBasic {
 //        MarioGame game = new MarioGame();
 //        SuperContraGame game = new SuperContraGame();
         PacManGame game = new PacManGame();
+
+        // We would now say that the specific game or the IGamingConsole
+        // is a dependency of the GameRunner class.
+        // Here we are "injecting" the dependency into the GameRunner class.
         GameRunner gameRunner = new GameRunner(game);
 
         gameRunner.run();
+
+        // Currently the object creation and the dependecy injection is written by us
+        // lets get to a stage where the Spring framework can handle this for us
+
+        // We want the spring framework to create these games for us and wire (inject dependencies)
+        // them together as well
+
     }
 }
