b5ae5171826088b333e2d8e42e85460d959eade9
Femi Ashiru <femi.ashiru@bjss.com>
2024-11-16 17:53:15 +0000

    add a bean
    adding bean means that this bean will be managed by the spring framework

--- src/main/java/com/myjavalearning/spring_framework/HelloWorldConfiguration.java
+++ src/main/java/com/myjavalearning/spring_framework/HelloWorldConfiguration.java
@@ -1,8 +1,14 @@
 package com.myjavalearning.spring_framework;
 //To indicate that this is a configuration class we add the @Configuration annotation
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class HelloWorldConfiguration {
+
+    @Bean
+    public String name(){
+        return "Sade";
+    }
 }
