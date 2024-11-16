e3c4f1414294971161f618d9348bd61e1a68caba
Femi Ashiru <femi.ashiru@bjss.com>
2024-11-16 17:38:55 +0000

    create new class to demo spring framework

---
+++ src/main/java/com/myjavalearning/spring_framework/App02HelloWorldSpring.java
@@ -0,0 +1,8 @@
+package com.myjavalearning.spring_framework;
+
+public class App02HelloWorldSpring {
+    public static void main(String[] args){
+        // 1: Launch a spring context
+        // 2: Configure the things we want spring framework to manage
+    }
+}
