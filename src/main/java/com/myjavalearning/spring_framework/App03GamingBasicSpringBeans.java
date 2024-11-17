5a34e8cda29da17b2905d29bb5269078a67a45b7
Femi Ashiru <femi.ashiru@bjss.com>
2024-11-17 02:37:13 +0000

    add context within try with resource
    add new context to use our GamingBasicConfiguration and place within try with resource as well as adding appropriate catch block

--- src/main/java/com/myjavalearning/spring_framework/App03GamingBasicSpringBeans.java
+++ src/main/java/com/myjavalearning/spring_framework/App03GamingBasicSpringBeans.java
@@ -2,10 +2,22 @@
 package com.myjavalearning.spring_framework;
 
 
+import com.myjavalearning.spring_framework.game.IGamingConsole;
+import org.springframework.beans.BeansException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class App03GamingBasicSpringBeans {
     public static void main(String[] args){
 
+        try(
+            AnnotationConfigApplicationContext context =
+                    new AnnotationConfigApplicationContext(GamingBasicConfiguration.class)
+        ){
 
+
+        }catch(BeansException ex){
+            System.out.println(ex.getMessage());
+        }
 
     }
 }
