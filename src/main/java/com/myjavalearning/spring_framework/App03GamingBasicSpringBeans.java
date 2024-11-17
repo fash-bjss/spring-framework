295b408d6117457bbd698a47aa4cf664e500d2fa
Femi Ashiru <femi.ashiru@bjss.com>
2024-11-17 02:27:33 +0000

    create new gaming basic to use Spring framework

---
+++ src/main/java/com/myjavalearning/spring_framework/App03GamingBasicSpringBeans.java
@@ -0,0 +1,11 @@
+// GameBasic now using Spring Framework
+package com.myjavalearning.spring_framework;
+
+
+public class App03GamingBasicSpringBeans {
+    public static void main(String[] args){
+
+
+
+    }
+}
