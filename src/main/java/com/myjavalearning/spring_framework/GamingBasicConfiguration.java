a5646f39d8176bf2c77f51834ed93451bd1baf4e
Femi Ashiru <femi.ashiru@bjss.com>
2024-11-17 02:32:32 +0000

    add a single game bean
    add pacman game bean using the interface as a return value

--- src/main/java/com/myjavalearning/spring_framework/GamingBasicConfiguration.java
+++ src/main/java/com/myjavalearning/spring_framework/GamingBasicConfiguration.java
@@ -1,7 +1,15 @@
 package com.myjavalearning.spring_framework;
 
+import com.myjavalearning.spring_framework.game.IGamingConsole;
+import com.myjavalearning.spring_framework.game.PacManGame;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GamingBasicConfiguration {
+
+    @Bean
+    public IGamingConsole game(){
+        return new PacManGame();
+    }
 }
